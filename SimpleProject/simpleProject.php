d90fade291c70de159b908e25c43d0d47ba27f0f
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-11-09 12:02:54 +0800

    Update simpleProject.php

--- SimpleProject/simpleProject.php
+++ SimpleProject/simpleProject.php
@@ -57,7 +57,7 @@
         function isPrime($num){
             if($num <= 1) return false;
 
-            if($num % 2 == 0) return false;
+            if($num / 2 == 0) return false;
 
             for($i = 2; $i <= sqrt($num); $i++){
                 if($num % $i == 0) return false;
